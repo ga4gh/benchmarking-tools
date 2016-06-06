6a95544908c0bf7ec67093dba44900a38c42628b
jzook <jzook@nist.gov>
2016-06-06 15:56:57 -0400

    Add decoy bed and new self-chain with ALT loci to SegmentalDuplications

--- resources/stratification-bed-files/SegmentalDuplications/selfchain_sort.sh
+++ resources/stratification-bed-files/SegmentalDuplications/selfchain_sort.sh
@@ -12,3 +12,11 @@ gunzip -c /Users/jzook/Documents/GA4GH/StratificationBedFiles/SegmentalDuplicati
 cat hg19_self_chain_split.1.bed hg19_self_chain_split.2.bed | sort -k1,1 -k2,2n | /Applications/bioinfo/bedtools2/bin/mergeBed -i stdin | bgzip -c > hg19_self_chain_split_both.bed.gz
 
 cat hg19_self_chain_split.1.bed hg19_self_chain_split.2.bed | awk '{if ($3-$2>=10000) print $0}' | sort -k1,1 -k2,2n | /Applications/bioinfo/bedtools2/bin/mergeBed -i stdin | bgzip -c > hg19_self_chain_split_both_gt10k.bed.gz
+
+
+#Create bed files with ALT loci included since original bed files did not include ALT mappings
+/Applications/bioinfo/bedtools2.25.0/bin/bamToBed -i /Users/jzook/Downloads/hg19_self_chain_split.bam | cut -f1-3 | /Applications/bioinfo/bedtools2.25.0/bin/mergeBed -i stdin | /Applications/bioinfo/tabix-0.2.6/bgzip -c > /Users/jzook/Downloads/hg19_self_chain_split_all.bed.gz
+gunzip -c /Users/jzook/Downloads/hg19_self_chain_split_all.bed.gz | awk '{if($3-$2>10000) sum+=$3;if($3-$2>10000) sum-=$2} END {print sum}' 
+#146352960
+gunzip -c /Users/jzook/Downloads/hg19_self_chain_split_all.bed.gz | awk '{if($3-$2>10000) print}' | /Applications/bioinfo/tabix-0.2.6/bgzip -c > /Users/jzook/Downloads/hg19_self_chain_split_withalts_gt10k.bed.gz
+
